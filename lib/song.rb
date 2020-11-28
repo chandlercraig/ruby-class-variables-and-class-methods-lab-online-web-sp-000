23ddd3255590202d549bd024cf1acb26b5484cc0
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-28 18:50:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,58 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@g_count_hash = {}
+  @@a_count_hash = {}
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+    @@genres
+  end
+
+  def self.artists
+    @@artists.uniq!
+    @@artists
+  end
+
+  def self.genre_count
+    @@genres.each do |song_genre|
+      if @@g_count_hash[song_genre]
+        @@g_count_hash.each {|genre_key, value| value+=1}
+      else
+        @@g_count_hash[song_genre]
+        @@g_count_hash[song_genre] = 1
+      end
+    end
+    @@g_count_hash
+  end
+
+  def self.artist_count
+    @@artists.each do |song_artist|
+      if @@a_count_hash[song_artist]
+        @@a_count_hash.each {|artist_key, value| value+=1}
+      else
+        @@a_count_hash[song_artist]
+        @@a_count_hash[song_artist] = 1
+      end
+    end
+    @@a_count_hash
+  end
+
+end
